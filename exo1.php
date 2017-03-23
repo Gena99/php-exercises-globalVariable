ec566a11f0066a3922677af6ad0ca3853f5355c0
Gena <gena@e64.net>
2017-03-23 11:22:00 +0100

    lancement premier exo

---
+++ exo1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Globales Variables</title>
+	<link rel="stylesheet" type="text/css" href="exo1.css">
+
+</head>
+<body>
+	<?php
+
+
+	echo $_SERVER['HTTP_USER_AGENT'];
+	echo $_SERVER["REMOTE_ADDR"];
+	echo $_SERVER["SERVER_NAME"];
+
+
+	?>
+	
+</body>
+</html>
